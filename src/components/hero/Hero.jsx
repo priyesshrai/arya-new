95f864dfd90248f3895ba0f01cf20cd36b19ade8
PriyeshRai369 <priyeshrai369@gmail.com>
2025-01-17 02:29:31 +0530

    update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,45 +10,73 @@ import Modal from "../modal/Modal";
 const people = [
   {
     id: 1,
-    name: "John Doe",
+    name: "Kashitij Huyndai",
     designation: "Software Engineer",
     image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+      "/images/testimonials/1.png",
   },
   {
     id: 2,
-    name: "Robert Johnson",
+    name: "Furniture Mart",
     designation: "Product Manager",
     image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+      "/images/testimonials/2.png",
   },
   {
     id: 3,
-    name: "Jane Smith",
+    name: "Banarsi Saree",
     designation: "Data Scientist",
     image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+      "/images/testimonials/3.png",
   },
   {
     id: 4,
-    name: "Emily Davis",
+    name: "Mastertech",
     designation: "UX Designer",
     image:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      "/images/testimonials/4.png",
   },
   {
     id: 5,
-    name: "Tyler Durden",
+    name: "Sunbeam Academy",
     designation: "Soap Developer",
     image:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+      "/images/testimonials/5.png",
   },
   {
     id: 6,
-    name: "Dora",
+    name: "Rimika",
     designation: "The Explorer",
     image:
-      "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+      "/images/testimonials/6.png",
+  },
+  {
+    id: 7,
+    name: "Dhanuka",
+    designation: "The Explorer",
+    image:
+      "/images/testimonials/7.png",
+  },
+  {
+    id: 8,
+    name: "Dirdhar Das ",
+    designation: "The Explorer",
+    image:
+      "/images/testimonials/8.png",
+  },
+  {
+    id: 9,
+    name: "Swarnam",
+    designation: "The Explorer",
+    image:
+      "/images/testimonials/9.png",
+  },
+  {
+    id: 10,
+    name: "Hero",
+    designation: "The Explorer",
+    image:
+      "/images/testimonials/10.png",
   },
 ];
 
